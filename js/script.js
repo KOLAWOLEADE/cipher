5ad6eaf287b88837a7bf35a6d48818684edb2ba5
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-06-04 16:00:06 +0100

    add letter to my function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,62 @@
 // Cipher function
 
-let x = (prompt("Write a sentence"));
+let sentence = prompt("Write a sentence");
+
+console.log();
+
 
 // invoke function
-function sentence (){
-    return x
+function letter(sentence){
+  let firstletter = sentence.charAt(0).toUpperCase();
+  let lastletter = sentence.charAt(sentence.length -1).toUpperCase();
+  let y = firstletter.concat(lastletter);
+    return y;
 }
 
-console.log(x)
+ alert(letter(sentence));
+ console.log(letter(sentence))
+ document.write(letter(sentence))
+
+ // invoke function on reverse
+ function reverse(){
+let  splitspring = y.split("")
+
+let rev = splitspring.reverse()
+
+let join = rev.join
+
+return join
+ }
+
+ console.log(reverse)
+
+
+
+
+
+// function charAT(y) {
+//   return x.charAt(0).toUpperCase()
+// }
+
+// console.log(charAT(0));
+
+// invoke function slice
+// function slice(){
+//   return x.slice(0).toUpperCase();
+// }
+// console.log(slice(0));
+
+// invoke function substring
+//  function substring(x){
+//       return x.substring(0,x.length-1)  
+//     }
+
+//   console.log(substring(x));
+
+// reverse method
+
+// function reverse(x){
+//    return reverse(x)
+// }
+// console.log (reverse)
+
