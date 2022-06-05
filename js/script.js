da25d17434d46e8413e9f0a5e48a4d3d2f7b2dc3
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-06-05 13:49:36 +0100

    add Original Sentence

--- js/script.js
+++ js/script.js
@@ -1,62 +1,185 @@
+$(document).ready(function(){
+    $(".sen").click(function(){
+        alert("sentence")
+    })
+
+
+    $(".enc").click(function(){
+        alert("newsentence")
+    })
+
+});
+
+
+//$(document).ready(function(){
+//     $(".original").click(function(){
+//         alert("sentence");
+
+//     });
+
+//     $(".encoded").click(function(){
+//         alert("newsentence");
+//     });
+
+
+// });
+
+   
+
+
+
+
+
+
 // Cipher function
 
-let sentence = prompt("Write a sentence");
+const sentence = prompt("Write a sentence");
 
-console.log();
+console.log(sentence);
+
+// To get the firstletter in a sentence
+let firstLetter = sentence.charAt(0).toUpperCase();
+
+// to get the lastletter
+let lastLetter = sentence.charAt(sentence.length -1).toUpperCase();
+
+// To concat first and last letter
+
+let firstAndLast = firstLetter.concat(lastLetter);
 
 
-// invoke function
-function letter(sentence){
-  let firstletter = sentence.charAt(0).toUpperCase();
-  let lastletter = sentence.charAt(sentence.length -1).toUpperCase();
-  let y = firstletter.concat(lastletter);
-    return y;
+// create  function to take the first letter and the last letter and return as a string in uppercase
+function firstAndLastLetter(){
+
+return firstAndLast;
+  
+}
+// involke my function
+
+ alert(firstAndLastLetter());
+ console.log(firstAndLastLetter());
+ document.write(firstAndLastLetter());
+
+ // function to reverse the order of my first and last letter
+ 
+ function reverseFirstAndLast(str){
+ // Step 1 use the split() method to return new array
+ let splitSpring = str.split("");
+
+
+ // Step 2 use the reverse() method to reverse the new created array
+
+ let reverseArray = splitSpring.reverse();
+
+ // Step 3 use the join () method to join() method to join all elements of the array into a string
+
+ let joinArray = reverseArray.join("");
+
+ // Step 4 Return the reversed string 
+
+ return joinArray;
+ }
+// calling my function with alert
+
+alert(reverseFirstAndLast(firstAndLast));
+console.log(reverseFirstAndLast(firstAndLast));
+document.write(reverseFirstAndLast(firstAndLast));
+
+// function that call on other two function
+
+function thirdFunction(){
+ let  firstFunction = firstAndLastLetter();
+ let  secondFunction = reverseFirstAndLast(firstAndLast);
+ let twoLetterstring = sentence.concat(secondFunction);
+ return twoLetterstring;
+}
+alert(thirdFunction());
+console.log(thirdFunction());
+document.write(thirdFunction());
+
+// counting original sentence
+
+function newsentence(){
+    let counting = Math.floor(sentence.length/2);
+    let y = sentence.charAt(counting);
+    let newsentence = y + " " + sentence
+
+
+    return newsentence;
 }
 
- alert(letter(sentence));
- console.log(letter(sentence))
- document.write(letter(sentence))
+alert(newsentence());
+console.log(newsentence());
+document.write(newsentence());
 
- // invoke function on reverse
- function reverse(){
-let  splitspring = y.split("")
+// Bonus reverse function
 
-let rev = splitspring.reverse()
+function reverseNewSentence(newsentence){
+// spliting before reverse
 
-let join = rev.join
+let A = newsentence.split("");
 
-return join
- }
+// taking the reverse of the splited sentence
 
- console.log(reverse)
+let rever = A.reverse();
+
+// joining the splited sentence and output it as reverse
+
+let binding = rever.join("");
+
+return binding;
+
+
+}
+
+alert(reverseNewSentence());
+console.log(reverseNewSentence());
+document.write(reverseNewSentence())
+
+// let reve = newsentence.reverse()
+
+
+
+//  // invoke function on reverse
+//  function reverse(){
+// let  splitspring = y.split("")
+
+// let rev = splitspring.reverse()
+
+// let join = rev.join
+
+// return join
+//  }
+
+//  console.log(reverse)
 
 
 
 
 
-// function charAT(y) {
-//   return x.charAt(0).toUpperCase()
-// }
+// // function charAT(y) {
+// //   return x.charAt(0).toUpperCase()
+// // }
 
-// console.log(charAT(0));
+// // console.log(charAT(0));
 
-// invoke function slice
-// function slice(){
-//   return x.slice(0).toUpperCase();
-// }
-// console.log(slice(0));
+// // invoke function slice
+// // function slice(){
+// //   return x.slice(0).toUpperCase();
+// // }
+// // console.log(slice(0));
 
-// invoke function substring
-//  function substring(x){
-//       return x.substring(0,x.length-1)  
-//     }
+// // invoke function substring
+// //  function substring(x){
+// //       return x.substring(0,x.length-1)  
+// //     }
 
-//   console.log(substring(x));
+// //   console.log(substring(x));
 
-// reverse method
+// // reverse method
 
-// function reverse(x){
-//    return reverse(x)
-// }
-// console.log (reverse)
+// // function reverse(x){
+// //    return reverse(x)
+// // }
+// // console.log (reverse)
 
